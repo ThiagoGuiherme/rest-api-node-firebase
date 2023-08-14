c9acce490ed9ea3a148f6f4bfe22d8fd6202afe3
Thiago Guilherme <thiago.ostrovski@aluno.unc.br>
2023-08-14 20:24:32 -0300

    criacao do model Book

---
+++ models/book.js
@@ -0,0 +1,16 @@
+class Book {
+    // implementando o construtor da classe
+constructor (
+    id,
+    title,
+    tituloOriginal,
+    genero,
+    anoLancamento
+) {
+    this.id = id
+    this.titulo = titulo,
+    this.tituloOriginal = tituloOriginal,
+    this.genero = genero,
+    this.anoLancamento = anoLancamento
+}
+}
